32eae42494a097817f00a5f1d1f5bab359134c9e
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-02-20 19:56:57 +0530

    Update Leap_year.java

--- Leap_year.java
+++ Leap_year.java
@@ -1,9 +1,9 @@
 package java_basics;
 
-import java.util.Scanner;                                       // IMPORTING SCANNER CLASS //
+import java.util.Scanner;                                              // IMPORTING SCANNER CLASS //
 public class Leap_year {
 
-	public static void main(String[] args) {          		    // ASSIGNING SCANNER CLASS INPUT SYSTEM OPENED //
+	public static void main(String[] args) {          	       // ASSIGNING SCANNER CLASS INPUT SYSTEM OPENED //
 		Scanner sc = new Scanner (System.in);
 		System.out.print("Enter Your Year :- ");                // ASSIGNING SHORT DATA TYPE //
 		short year = sc.nextShort();                            // NESTED IF AND IF ELSE STATEMENTS OPENED //
@@ -23,9 +23,9 @@ public class Leap_year {
 			else {
 				System.out.println(year+"is not a Leap year");
 			}
-		}                                                        // NESTED IF AND IF ELSE STATEMENTS CLOSED //
+		}                                                                // NESTED IF AND IF ELSE STATEMENTS CLOSED //
 		
-		sc.close();                                             // SCANNER CLASS CLOSED //
+		sc.close();                                                      // SCANNER CLASS CLOSED //
 		
 
 	}
