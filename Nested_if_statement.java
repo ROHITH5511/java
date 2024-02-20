e2f9d94adcce45a7d68f178ba11e73391b78312d
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-02-20 19:10:45 +0530

    Update Nested_if_statement.java

--- Nested_if_statement.java
+++ Nested_if_statement.java
@@ -1,18 +1,21 @@
 package java_basics;
 
-// IMPORTED SCANNER CLASS//
+// IMPORTING SCANNER CLASS//
+
 import java.util.Scanner;
 public class Nested_if_statement {
 
 	public static void main(String[] args) {
-		// ASSIGNED SCANNER CLASS INPUT SYSTEM //
+		// ASSIGNING SCANNER CLASS INPUT SYSTEM //
+		
 		Scanner sc = new Scanner (System.in);
 		System.out.print("enter your number :-  ");
 		
-		// ASSIGNED INTEGER DATA TYPE //
+		// ASSIGNING INTEGER DATA TYPE //
 		int number = sc.nextInt();
 		
 		// NESTED IF STATEMENT STARTED // 
+		
 		if(number%5==0) {
 			if(number%11==0) {
 				System.out.println("divisible by 5 & 11");
@@ -22,6 +25,7 @@ public class Nested_if_statement {
 			}
 		}
 		// NESTED IF STATEMENT CLOSED //
+			
 		else {
 			System.out.println("not divisible by 5 & 11" );
 		}
