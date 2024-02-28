904414950b01272363ddcd2a1c506d22620c67ce
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-02-28 17:31:11 +0530

    Add files via upload

--- For_loop.java
+++ For_loop.java
@@ -1,10 +1,12 @@
+// print numbers from 10 to 1 //
+
 package Loop_concepts;
 
 public class For_loop {
-
+	
 	public static void main(String[] args) {
-		for (byte i = 1;i<=10;i++) {
-			System.out.println(i);
+		for (byte i = 10;i>=1;--i) {
+			System.out.print(i+",");
 		}
 
 	}
