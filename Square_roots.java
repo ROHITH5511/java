012ee8eac35827aca0178cb8dcb25b62accd7797
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-03-01 18:46:34 +0530

    Add files via upload

---
+++ Square_roots.java
@@ -0,0 +1,13 @@
+package Loop_concepts;
+
+public class Square_roots {
+
+	public static void main(String[] args) {
+		System.out.println("  number " +"  " +" square root");
+		for(int i = 1; i<=10; i++) {
+			System.out.println("   "+ i+ " \t\t " +i*i);
+		}
+
+	}
+
+}
