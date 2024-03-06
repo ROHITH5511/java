5c5ff8c43e506e55755a62a7e4a98f2720d94d60
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-03-06 20:24:23 +0530

    Add files via upload

---
+++ Whileloop_Print_numbers.java
@@ -0,0 +1,19 @@
+package Loop_concepts;
+
+import java.util.Scanner;
+public class Whileloop_Print_numbers {
+
+	public static void main(String[] args) {
+		Scanner rk = new Scanner(System.in);
+		System.out.println("Enter the first number :- ");
+		int first_number = rk.nextInt();
+		System.out.println("Enter your limit :- ");
+		int limit_number = rk.nextInt();
+		while(first_number<=limit_number) {
+			System.out.println(first_number);
+			first_number++;
+		}
+
+	}
+
+}
