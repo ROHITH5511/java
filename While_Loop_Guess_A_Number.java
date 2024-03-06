2e448b0060ec2e16ca122039e44d21f286ebfd3f
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-03-06 20:57:04 +0530

    Add files via upload

--- While_Loop_Guess_A_Number.java
+++ While_Loop_Guess_A_Number.java
@@ -1,16 +1,16 @@
 package Loop_concepts;
-import java.util.Scanner;
+import java.util.Scanner;                                    // IMPORTED SCANNER CLASS //
 public class While_Loop_Guess_A_Number {
 
 	public static void main(String[] args) {
 		Scanner rk = new Scanner (System.in);
-		System.out.println("Enter your SECRETE number :- ");
-		int secrete_number = rk.nextInt();
+		System.out.println("Enter your SECRETE number :- "); 
+		int secrete_number = rk.nextInt();                     // INITIATED INTEGER DATA TYPE AND SCANNER CLASS FOR SECRETE NUMBER //
 		
 		System.out.println("Enter your GUESS number :- ");
-		int guess_number = rk.nextInt();
+		int guess_number = rk.nextInt();                        // INITIATED INTEGER DATA TYPE AND SCANNER CLASS FOR GUESSING A  NUMBER //
 		
-		while(guess_number!=secrete_number) {
+		while(guess_number!=secrete_number) {                    // WHILE LOOP INITIATED //
 			if(guess_number>secrete_number) {
 				System.out.println(guess_number+" GUESS number is HIGHER");
 			}
@@ -18,11 +18,11 @@ public class While_Loop_Guess_A_Number {
 				System.out.println(guess_number+" Guess number is LOWER");
 			}
 			System.out.println("guess again");
-			guess_number = rk.nextInt();
+			guess_number = rk.nextInt();                         // INITIATED INTEGER DATA TYPE AND SCANNER CLASS FOR GUESSING A NUMBER AGAIN //
 		}
 		
 		System.out.println(guess_number+" CONGRATULATIONS YOUR GUESS IS RIGHT");
-		rk.close();
+		rk.close();                                              // SCANNER CLASS CLOSED //
 
 	}
 
