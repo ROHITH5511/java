aa526ffad3a27553037240fc98f4bc4c7c4146c0
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-03-10 18:55:08 +0530

    Add files via upload

---
+++ LOOP_CONCEPT_PRINT_ALPHABETS.java
@@ -0,0 +1,13 @@
+/* Write a program to print all alphabets from a to z using for loop?  */
+
+package Loop_concepts;
+
+public class LOOP_CONCEPT_PRINT_ALPHABETS {
+
+	public static void main(String[] args) {
+		for (char alpha = 'a'; alpha <= 'z'; alpha++) {
+            System.out.print(alpha+" ");	            
+        }
+	}
+
+}
