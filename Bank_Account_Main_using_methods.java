e39f85ca70cebb791a345f8356846ac6c82fe579
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-03-31 18:08:01 +0530

    Add files via upload

--- Bank_Account_Main_using_methods.java
+++ Bank_Account_Main_using_methods.java
@@ -1,13 +1,21 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class Bank_Account_Main_using_methods {
 
 	public static void main(String[] args) {
+		Scanner rk = new Scanner (System.in);
+		
 		Bank_Account_methods obj = new Bank_Account_methods();
+		System.out.println("enter your deposit amount: ");
+		int amount = rk.nextInt();
+		System.out.println("enter the amount to withdrawl: ");
+		int withdrawl_amount = rk.nextInt();
 		double initial_amount = 0;
 		System.out.println("Intial amount = "+initial_amount);
-		System.out.println("Rupees "+obj.deposit(10000)+" deposited successfully");
-		System.out.println("Withdrawl rupees "+obj.withdrawl(1000)+" successfully");
+		System.out.println("Rupees "+obj.deposit(amount)+" deposited successfully");
+		System.out.println("Withdrawl rupees "+obj.withdrawl(withdrawl_amount)+" successfully");
 		System.out.println("Balance = "+obj.get_balance());
 		
 
