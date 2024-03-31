4e4a4f46a0864a8fb59fc2052c39e3356f755978
ROHITH5511 <104890520+ROHITH5511@users.noreply.github.com>
2024-03-31 18:09:12 +0530

    Update Bank_Account_Main_using_methods.java

--- Bank_Account_Main_using_methods.java
+++ Bank_Account_Main_using_methods.java
@@ -8,10 +8,13 @@ public class Bank_Account_Main_using_methods {
 		Scanner rk = new Scanner (System.in);
 		
 		Bank_Account_methods obj = new Bank_Account_methods();
+		
 		System.out.println("enter your deposit amount: ");
 		int amount = rk.nextInt();
+		
 		System.out.println("enter the amount to withdrawl: ");
 		int withdrawl_amount = rk.nextInt();
+		
 		double initial_amount = 0;
 		System.out.println("Intial amount = "+initial_amount);
 		System.out.println("Rupees "+obj.deposit(amount)+" deposited successfully");
